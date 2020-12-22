dcd865bba79f499d9be58e2a8b07af90645bdec2
Christian <christianheyn@mailbox.org>
2020-12-22 12:52:13 +0100

    file to AST in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,33 @@
 module Main where
     import Text.RawString.QQ
     import Tokenizer
+    import System.Environment
+    import System.Directory
+    import qualified Data.ByteString.Lazy.Char8 as L
+    import Tokenizer
+    import Syntax
 
-    code = [r|
+    noColor = "\x1b[0m"
+    underline = "\x1b[4m"
 
-    }
-    |]
+    red = "\x1b[31m"
+    magenta = "\x1b[35m"
+    yellow = "\x1b[33m"
+    green = "\x1b[32m"
+    cyan = "\x1b[36m"
+    grey = "\x1b[2m"
+    whiteBg = "\x1b[7m"
+
+    fileAst = do
+        (filepath:_) <- getArgs
+        cwd <- getCurrentDirectory
+        x <- readFile $ cwd ++ "/" ++ filepath
+        let ast = isTopLevel $ generateTokens (L.pack x)
+            valid = if length (snd ast) /= 0
+                    then red ++ "--- Error ---" ++ noColor
+                    else green ++ "--- Valid ---" ++ noColor
+        print ast
+        putStrLn valid
 
     main :: IO ()
-    main = print $ tokenize code
+    main = fileAst
